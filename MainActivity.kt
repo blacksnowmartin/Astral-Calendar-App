8ccf7f08d7288b25ef078b92e23ccf12cf4d6b45
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-10-21 23:45:01 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,13 +1,7 @@
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import java.text.SimpleDateFormat
-import java.util.Calendar
-
-import android.content.res.Configuration
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,56 +12,11 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        // Add any additional code you need for your app
-    }
+        // Add animation for the app logo
+        val appLogo = findViewById<ImageView>(R.id.appLogo)
+        val fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_animation)
+        appLogo.startAnimation(fadeAnimation)
 
-    // Method to toggle dark and light mode
-    private fun toggleDarkLightMode() {
-        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-        val newNightMode = when (currentNightMode) {
-            Configuration.UI_MODE_NIGHT_NO -> AppCompatDelegate.MODE_NIGHT_YES
-            Configuration.UI_MODE_NIGHT_YES -> AppCompatDelegate.MODE_NIGHT_NO
-            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-        }
-        AppCompatDelegate.setDefaultNightMode(newNightMode)
-        recreate()
-    }
-}
-
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Get the current Gregorian date
-        val currentDate = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
-        val formattedDate = dateFormat.format(currentDate.time)
-
-        // Display the Gregorian date in your TextView
-        val gregorianDateTextView = findViewById<TextView>(R.id.gregorianDate)
-        gregorianDateTextView.text = formattedDate
-
-        // Calculate the superscript number
-        val superscriptNumber = calculateSuperscript(formattedDate)
-
-        // Display the superscript number in your TextView
-        val superscriptNumberTextView = findViewById<TextView>(R.id.superscriptNumber)
-        superscriptNumberTextView.text = superscriptNumber.toString()
-    }
-
-    // Function to calculate the superscript number
-    private fun calculateSuperscript(date: String): Int {
-        var sum = 0
-        date.forEach {
-            if (it.isDigit()) {
-                sum += it.toString().toInt()
-            }
-        }
-        while (sum > 9) {
-            sum = sum.toString().map { it.toString().toInt() }.sum()
-        }
-        return sum
+        // Add any additional code for your app
     }
 }
