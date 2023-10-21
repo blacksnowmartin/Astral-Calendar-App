976eb07983dfe9cc7361e28eb34d25e53f0b7567
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-10-21 23:48:57 +0300

    Update MainActivity.kt
    Comprehensive code for all functions.

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,7 +1,11 @@
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +16,44 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        // Calculate and display the Gregorian date and astral date
+        calculateAndDisplayDates()
+
         // Add animation for the app logo
         val appLogo = findViewById<ImageView>(R.id.appLogo)
         val fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_animation)
         appLogo.startAnimation(fadeAnimation)
+    }
 
-        // Add any additional code for your app
+    private fun calculateAndDisplayDates() {
+        // Get the current Gregorian date
+        val currentDate = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+        val formattedDate = dateFormat.format(currentDate.time)
+
+        // Display the Gregorian date in your TextView
+        val gregorianDateTextView = findViewById<TextView>(R.id.gregorianDate)
+        gregorianDateTextView.text = formattedDate
+
+        // Calculate the superscript number
+        val superscriptNumber = calculateSuperscript(formattedDate)
+
+        // Display the superscript number in your TextView
+        val superscriptNumberTextView = findViewById<TextView>(R.id.superscriptNumber)
+        superscriptNumberTextView.text = superscriptNumber.toString()
+    }
+
+    // Function to calculate the superscript number
+    private fun calculateSuperscript(date: String): Int {
+        var sum = 0
+        date.forEach {
+            if (it.isDigit()) {
+                sum += it.toString().toInt()
+            }
+        }
+        while (sum > 9) {
+            sum = sum.toString().map { it.toString().toInt() }.sum()
+        }
+        return sum
     }
 }
